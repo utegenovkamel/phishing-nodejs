1b0b5ae6afe5cff16c19a4aa81dcd919edb7f625
Kamel <kamel_utegeno@mail.ru>
2021-05-08 04:23:27 +0600

    register sequelize, create start func

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const router = require('./routes')
-const fileUpload = require('express-fileupload')
-
 require('dotenv').config()
+const express = require('express')
+const fileUpload = require('express-fileupload')
+const sequelize = require('./db')
+const router = require('./routes')
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -11,4 +11,14 @@ app.use(express.json())
 app.use(fileUpload())
 app.use('/api', router)
 
-app.listen(PORT, () => console.log(`App started on port ${PORT}`))
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`App started on port ${PORT}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
